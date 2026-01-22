5dd02fe260568397eacb88379423c5a90fab4f7f
WAYNE-BOSS\WAYNE <fplls12@gmail.com>
2026-01-22 15:27:28 +0900

    FEAT : 재귀 합, 피보나치 수열

---
+++ DataStructure/MyRecursion.h
@@ -0,0 +1,83 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+void Func2()
+{
+	int i = 0;
+	cout << "is Func2 " << endl;
+}
+
+void Func1()
+{
+	int j = 1;
+	Func2();
+	cout << "is Func1" << endl;
+
+}
+
+void RecursionFunc(int _iCount)
+{
+
+	if (_iCount == 0)
+		return;
+
+	RecursionFunc(_iCount - 1); // 함수 호출 시점에 따라서 결과가 달라지게 된다.
+
+	cout << _iCount << endl;
+
+	//RecursionFunc(_iCount - 1);
+}
+
+
+inline int RecursionSum(int* _iArr, int iSize)
+{
+	int iIndex = iSize - 1;
+
+	if (iIndex < 0)
+		return 0;
+
+	int iSum = RecursionSum(_iArr, iIndex) + _iArr[iIndex];
+
+	return iSum;
+
+	// 재귀를 호출을 하면 스택에 쌓이는 메모리가 보다 많다. 
+	// 공간 복잡도 O(N), 시간 복잡도 O(N)
+}
+
+
+
+inline int Fibonacci(int iValue)
+{
+	if (iValue == 0)
+		return 0;
+	else if (iValue == 1)
+		return 1;
+	else
+	{
+		int iPrev1 = 0;
+		int iPrev2 = 1;
+		int iTemp = 0;
+
+		for (int i = 2; i < iValue + 1; i++)
+		{
+			iTemp = iPrev1 + iPrev2;
+			iPrev1 = iPrev2;
+			iPrev2 = iTemp;
+		}
+
+		return iTemp;
+	}
+}
+
+inline int RecursionFibonacci(int iValue)
+{
+	if (iValue == 0)
+		return 0;
+	else if (iValue == 1)
+		return 1;
+
+	return RecursionFibonacci(iValue - 1) + RecursionFibonacci(iValue - 2);
+
+}
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -1,17 +1,29 @@
-#include <iostream>
-#include "MySort.h"
-#include "MySearch.h"
+#include "MyRecursion.h"
 
 using namespace std;
 
-
-
 int main()
 {
-	int iArr[] = { 0,1,2,3,4,5,6,7,8,9 };
+	int iArr[] = { 0,1,2,3,4,5,6,7,8,9,10,11 };
+				// 0,1,1,2,3,5,8,13,21,34,55
 	int iSize = sizeof(iArr) / sizeof(iArr[0]);
+	
+	cout << "Fibonacci : ";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << " " << Fibonacci(i) << " ";
+	}
+	
+	cout << endl;
 
-	BinarySearch(iArr,iSize,5);
+	cout << "Fibonacci : ";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << " " << RecursionFibonacci(i) << " ";
+	}
+
+	cout << endl;
+
 
 
 	return 0;
