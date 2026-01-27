2c879738d996366fae0311cbf1b117fe36d3f7bc
WAYNE-BOSS\WAYNE <fplls12@gmail.com>
2026-01-27 20:42:50 +0900

    FEAT : 미로 문제 구현중

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -3,30 +3,78 @@
 
 using namespace std;
 
-int main()
+constexpr int iNumRows = 10;
+constexpr int iNumCols = 9;
+
+char szMaze[iNumRows][iNumCols] =
 {
-	// 문자열 뒤집기
+	{ '1', '1', '1', '1', '1', '1', '1', '1', '1' },
+	{ '1', 'S', '0', '0', '0', '0', '0', '0', '1' },
+	{ '1', '0', '0', '0', '0', '0', '0', '0', '1' },
+	{ '1', '0', '1', '1', '1', '1', '1', '1', '1' },
+	{ '1', '0', '0', '0', '0', '0', '0', '0', '1' },
+	{ '1', '1', '1', '1', '1', '0', '1', '1', '1' },
+	{ '1', '1', '1', '1', '1', '0', '1', '1', '1' },
+	{ '1', '1', '1', '0', '0', '0', '1', '0', '1' },
+	{ '1', '0', '0', '0', '0', '0', '0', 'G', '1' },
+	{ '1', '1', '1', '1', '1', '1', '1', '1', '1' },
+};
 
-	char szArr[] = "Hello World!";
-	int iSize = sizeof(szArr) - 1;
 
-	MyStack<char> Stk(iSize);
-
-	cout << "Input : " << szArr << endl;
-
-	for (int i = 0; i < iSize; i++)
+void PrintMaze()
+{
+	for (int i = 0; i < iNumRows; i++)
 	{
-		Stk.Push_Stack(szArr[i]);
+		for (int j = 0; j < iNumCols; j++)
+		{
+			cout << szMaze[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+typedef struct tagMazePosition
+{
+	int iRow = 0;
+	int iCol = 0;
+
+}MAZEPOS;
+
+void RecurMaze(MAZEPOS Position)
+{
+	int iRow = Position.iRow;
+	int iCol = Position.iCol;
+
+	const char cMark = szMaze[Position.iRow][Position.iCol];
+
+	if (szMaze[iRow][iCol] == 'G')
+	{
+		cout << "Found!" << endl;
+		return;
 	}
 
-
-	for (int i = 0; i < iSize; i++)
+	if (szMaze[iRow][iCol] != '1' && szMaze[iRow][iCol] != 'X')
 	{
-		cout << Stk.Top();
-		Stk.Pop_Stack();
+		szMaze[iRow][iCol] = 'X';
+		RecurMaze({ iRow - 1, iCol });
+		RecurMaze({ iRow + 1, iCol });
+		RecurMaze({ iRow, iCol - 1 });
+		RecurMaze({ iRow, iCol + 1 });
 	}
 	
+}
+
+
+int main()
+{
+	MAZEPOS Pos{};
+	Pos.iRow = 1;
+	Pos.iCol = 1;
+
+	PrintMaze();
+	RecurMaze(Pos);
 	cout << endl;
+	PrintMaze();
 
 	return 0;
 }
