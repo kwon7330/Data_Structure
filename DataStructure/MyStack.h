04aff75952eeade24a6502cd19e982edaf4a77db
WAYNE-BOSS\WAYNE <fplls12@gmail.com>
2026-01-28 14:59:19 +0900

    FEAT : 하노이 탑 구현

--- DataStructure/MyStack.h
+++ DataStructure/MyStack.h
@@ -132,3 +132,135 @@ inline void MyStack<T>::Pop_Stack()
 //cout << endl;
 
 #pragma endregion
+
+
+#pragma region 미로 찾기
+
+constexpr int iNumRows = 10;
+constexpr int iNumCols = 9;
+
+char szMaze[iNumRows][iNumCols] =
+{
+	{ '1', '1', '1', '1', '1', '1', '1', '1', '1' },
+	{ '1', 'S', '0', '0', '0', '0', '0', '0', '1' },
+	{ '1', '0', '0', '0', '0', '0', '0', '0', '1' },
+	{ '1', '0', '1', '1', '1', '1', '1', '1', '1' },
+	{ '1', '0', '0', '0', '0', '0', '0', '0', '1' },
+	{ '1', '1', '1', '1', '1', '0', '1', '1', '1' },
+	{ '1', '1', '1', '1', '1', '0', '1', '1', '1' },
+	{ '1', '1', '1', '0', '0', '0', '1', '0', '1' },
+	{ '1', '0', '0', '0', '0', '0', '0', 'G', '1' },
+	{ '1', '1', '1', '1', '1', '1', '1', '1', '1' },
+};
+
+
+void PrintMaze()
+{
+	for (int i = 0; i < iNumRows; i++)
+	{
+		for (int j = 0; j < iNumCols; j++)
+		{
+			cout << szMaze[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+typedef struct tagMazePosition
+{
+	int iRow = 0;
+	int iCol = 0;
+
+}MAZEPOS;
+
+void RecurMaze(MAZEPOS Position)
+{
+	int iRow = Position.iRow;
+	int iCol = Position.iCol;
+
+	const char cMark = szMaze[Position.iRow][Position.iCol];
+
+	if (szMaze[iRow][iCol] == 'G')
+	{
+		cout << "Found!" << endl;
+		return;
+	}
+
+	if (szMaze[iRow][iCol] != '1' && szMaze[iRow][iCol] != 'X')
+	{
+		szMaze[iRow][iCol] = 'X';
+		RecurMaze({ iRow - 1, iCol });
+		RecurMaze({ iRow + 1, iCol });
+		RecurMaze({ iRow, iCol - 1 });
+		RecurMaze({ iRow, iCol + 1 });
+	}
+
+}
+#pragma endregion
+
+#pragma region 하노이 탑
+
+MyStack<char> Tower[3];
+
+void PrintTowers()
+{
+	cout << "Towers" << endl;
+	cout << "0 : ";
+	Tower[0].Print_Stack();
+	cout << "1 : ";
+	Tower[1].Print_Stack();
+	cout << "2 :";
+	Tower[2].Print_Stack();
+
+}
+void MoveDisk(int _iFrom, int _iTo)
+{
+	if (Tower[_iFrom].isEmpty())
+	{
+		cout << "Tower" << _iFrom << "is Empty" << endl;
+		return;
+	}
+
+	int iSelectedDisk = Tower[_iFrom].Top();
+
+	if (!Tower[_iTo].isEmpty() && Tower[_iTo].Top() > iSelectedDisk)
+	{
+		cout << " Cant Move Disk " << _iFrom << " To " << _iTo;
+		return;
+	}
+
+	Tower[_iFrom].Pop_Stack();
+	Tower[_iTo].Push_Stack(iSelectedDisk);
+
+	cout << "Disk Moved " << _iFrom << " To " << _iTo << endl;
+
+}
+
+void RecurMoveDisk(int _iNumDisk, int _iFrom, int _iTemp, int _iTo)
+{
+
+	if (_iNumDisk == 0)
+		return;
+
+	RecurMoveDisk(_iNumDisk - 1, _iFrom, _iTo, _iTemp);
+	MoveDisk(_iFrom, _iTo);
+	RecurMoveDisk(_iNumDisk - 1, _iTemp, _iFrom, _iTo);
+
+
+
+	/*int iNumDisks = 3;
+
+	for (int i = 0; i < iNumDisks; i++)
+	{
+		Tower[0].Push_Stack(i + 'A');
+	}
+
+	PrintTowers();
+
+	RecurMoveDisk(iNumDisks, 0, 1, 2);
+
+	PrintTowers();*/
+
+
+}
+#pragma endregion
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -3,78 +3,13 @@
 
 using namespace std;
 
-constexpr int iNumRows = 10;
-constexpr int iNumCols = 9;
 
-char szMaze[iNumRows][iNumCols] =
-{
-	{ '1', '1', '1', '1', '1', '1', '1', '1', '1' },
-	{ '1', 'S', '0', '0', '0', '0', '0', '0', '1' },
-	{ '1', '0', '0', '0', '0', '0', '0', '0', '1' },
-	{ '1', '0', '1', '1', '1', '1', '1', '1', '1' },
-	{ '1', '0', '0', '0', '0', '0', '0', '0', '1' },
-	{ '1', '1', '1', '1', '1', '0', '1', '1', '1' },
-	{ '1', '1', '1', '1', '1', '0', '1', '1', '1' },
-	{ '1', '1', '1', '0', '0', '0', '1', '0', '1' },
-	{ '1', '0', '0', '0', '0', '0', '0', 'G', '1' },
-	{ '1', '1', '1', '1', '1', '1', '1', '1', '1' },
-};
-
-
-void PrintMaze()
-{
-	for (int i = 0; i < iNumRows; i++)
-	{
-		for (int j = 0; j < iNumCols; j++)
-		{
-			cout << szMaze[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
-typedef struct tagMazePosition
-{
-	int iRow = 0;
-	int iCol = 0;
-
-}MAZEPOS;
-
-void RecurMaze(MAZEPOS Position)
-{
-	int iRow = Position.iRow;
-	int iCol = Position.iCol;
-
-	const char cMark = szMaze[Position.iRow][Position.iCol];
-
-	if (szMaze[iRow][iCol] == 'G')
-	{
-		cout << "Found!" << endl;
-		return;
-	}
-
-	if (szMaze[iRow][iCol] != '1' && szMaze[iRow][iCol] != 'X')
-	{
-		szMaze[iRow][iCol] = 'X';
-		RecurMaze({ iRow - 1, iCol });
-		RecurMaze({ iRow + 1, iCol });
-		RecurMaze({ iRow, iCol - 1 });
-		RecurMaze({ iRow, iCol + 1 });
-	}
-	
-}
 
 
 int main()
 {
-	MAZEPOS Pos{};
-	Pos.iRow = 1;
-	Pos.iCol = 1;
 
-	PrintMaze();
-	RecurMaze(Pos);
-	cout << endl;
-	PrintMaze();
+
 
 	return 0;
 }
